ff009ad8ae9d6047cbc65c4508eed57ffde6dd79
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-05 18:06:09 +0100

    wip: Use Krawler image in the build process #4

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 source .travis.env.sh
 
-echo Building k-teleray $VERSION with Krawler $KRAWLER_BRANCH
+echo Building k-teleray $TAG with Krawler $KRAWLER_TAG
 
 # Build image
-docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile -t kalisio/k-teleray .
-docker tag kalisio/k-teleray kalisio/k-teleray:$VERSION
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile -t kalisio/k-teleray:$TAG .
 
 # Push the built images to Docker hub
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-docker push kalisio/k-teleray:$VERSION
+docker push kalisio/k-teleray:$TAG
--- .travis.env.sh
+++ .travis.env.sh
@@ -3,9 +3,9 @@
 # Build docker with version number only on release
 if [[ -z "$TRAVIS_TAG" ]]
 then
-	export VERSION=latest
+	export TAG=latest
 	export KRAWLER_TAG=latest
 else
-	export VERSION=$(node -p -e "require('./package.json').version")
+	export TAG=$(node -p -e "require('./package.json').version")
 	export KRAWLER_TAG=v$(node -p -e "require('./package.json').peerDependencies['@kalisio/krawler']")
 fi

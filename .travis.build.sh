34bf9d1d9d2b12e96b54ba1815a7887ec609cfa1
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-13 14:44:45 +0100

    ci: cleaned build scripts

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source .travis.env.sh
 
 # Source the environment to define Krawler and image versions
 source .travis.env.sh
@@ -7,7 +6,7 @@ echo Building $IMAGE_NAME:$IMAGE_TAG with Krawler-$KRAWLER_TAG
 
 # Build the image
 docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
+
 # Publish the image
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 docker push $IMAGE_NAME:$IMAGE_TAG
-

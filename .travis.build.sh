e59a5d70641677bc7640082a3727df02c55bcc46
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-13 10:40:51 +0100

    ci: updated CI scripts

--- .travis.build.sh
+++ .travis.build.sh
@@ -8,5 +8,6 @@ echo Building $IMAGE_NAME:$IMAGE_TAG with Krawler-$KRAWLER_TAG
 # Build the image
 docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
 # Publish the image
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 docker push $IMAGE_NAME:$IMAGE_TAG
 
